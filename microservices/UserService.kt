1659e16d43981366a30413b8722fb0904f9de535
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-09 00:23:32 -0700

    Update UserService.kt

--- microservices/UserService.kt
+++ microservices/UserService.kt
@@ -9,11 +9,46 @@ import io.ktor.response.respond
 import io.ktor.routing.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.transactionManager
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.SchemaUtils.drop
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 
 data class User(val id: Int, val name: String, val email: String)
 
+object Users : Table() {
+    val id = integer("id").autoIncrement().primaryKey()
+    val name = varchar("name", 50)
+    val email = varchar("email", 50)
+}
+
+object DatabaseFactory {
+    fun init() {
+        val config = HikariConfig().apply {
+            jdbcUrl = "jdbc:postgresql://localhost:5432/mydatabase"
+            driverClassName = "org.postgresql.Driver"
+            username = "dbuser"
+            password = "dbpassword"
+            maximumPoolSize = 3
+            isAutoCommit = false
+            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        }
+        val dataSource = HikariDataSource(config)
+        Database.connect(dataSource)
+
+        transaction {
+            create(Users)
+        }
+    }
+}
+
 object UserService {
-    private val users = mutableListOf<User>()
+    init {
+        DatabaseFactory.init()
+    }
 
     fun start() {
         embeddedServer(Netty, port = 8080) {
@@ -26,19 +61,35 @@ object UserService {
             routing {
                 route("/users") {
                     get {
+                        val users = transaction {
+                            Users.selectAll().map {
+                                User(
+                                    id = it[Users.id],
+                                    name = it[Users.name],
+                                    email = it[Users.email]
+                                )
+                            }
+                        }
                         call.respond(users)
                     }
 
                     post {
                         val user = call.receive<User>()
-                        users.add(user)
+                        transaction {
+                            Users.insert {
+                                it[name] = user.name
+                                it[email] = user.email
+                            }
+                        }
                         call.respond(HttpStatusCode.Created, user)
                     }
 
                     delete("/{id}") {
                         val id = call.parameters["id"]?.toIntOrNull()
                         if (id != null) {
-                            users.removeIf { it.id == id }
+                            transaction {
+                                Users.deleteWhere { Users.id eq id }
+                            }
                             call.respond(HttpStatusCode.NoContent)
                         } else {
                             call.respond(HttpStatusCode.BadRequest)
